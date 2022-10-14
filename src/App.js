f912c5aaf82c95be0b05cf18d397df62560bb4b9
miseon <miseon920@naver.com>
2022-10-14 09:36:34 +0900

    20221014

---
+++ src/App.js
@@ -0,0 +1,21 @@
+const ThedeepSub = () => {
+  return <>ThedeepSub</>;
+};
+
+const DeepSub = () => {
+  return <>DeepSub</>;
+};
+
+const Sub = () => {
+  return <>sub</>;
+};
+
+function App() {
+  return (
+    <div className="App">
+      <Sub />
+    </div>
+  );
+}
+
+export default App;
